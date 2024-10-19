d1746317e80514f557db090035fa99e2d7f27fd8
rizkiwhy <rizkiiiwahyudi@gmail.com>
2024-10-19 17:41:16 +0700

    feat(books): get list

--- src/router/public.ts
+++ src/router/public.ts
@@ -3,3 +3,4 @@ import { BookController } from "../controller/book.controller"
 
 export const publicRouter = express.Router()
 publicRouter.post('/books', BookController.createBook)
+publicRouter.get('/books', BookController.getBooks)
